5d99f3325cf649ae3c74416a04b5836538890027
huangzehai <chaogeek@foxmail.com>
2018-02-24 16:52:42 +0800

    Add EventTimeWindowWordCount example

--- src/main/scala/EventTimeWindowWordCountExample.scala
+++ src/main/scala/EventTimeWindowWordCountExample.scala
@@ -12,7 +12,7 @@ object EventTimeWindowWordCountExample {
 
 
     import spark.implicits._
-    val df = Seq(("Hello", "2018-02-24 16:35:09"), ("world", "2018-02-24 16:35:09"), ("Hello", "2018-02-24 16:35:09"), ("Adam", "2018-02-24 16:35:09")).toDF("word", "ts")
+    val df = Seq(("Hello", "2018-02-24 16:35:09"), ("world", "2018-02-24 16:35:09"), ("Hello", "2018-02-24 16:35:09"), ("Adam", "2018-02-24 16:45:09")).toDF("word", "ts")
     import org.apache.spark.sql.functions.unix_timestamp
     val ts = unix_timestamp($"ts", "yyyy-MM-dd HH:mm:ss")
     //替换列
@@ -20,11 +20,11 @@ object EventTimeWindowWordCountExample {
 
     // Group the data by window and word and compute the count of each group
     import org.apache.spark.sql.functions._
-    val windowedCounts = df.groupBy(window($"ts", "10 minutes", "5 minutes"),
+    val windowedCounts = df.groupBy(window($"ts", "10 minutes", "2 minutes"),
       $"word"
     ).count()
 
-    windowedCounts.show
+    windowedCounts.collect().foreach(println)
   }
 
   case class Word(word: String, timestamp: TimestampType);
