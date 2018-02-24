6d8d095a8f1c7ed6cd69324e59d648c128fa9a3c
huangzehai <chaogeek@foxmail.com>
2018-02-24 17:39:11 +0800

    Add FileSourceStreamWordCount example

---
+++ src/main/scala/FileSourceStreamWordCount.scala
@@ -0,0 +1,30 @@
+import org.apache.spark.sql.SparkSession
+
+object FileSourceStreamWordCount {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("FileSourceStreamWordCount")
+      .getOrCreate()
+
+    import spark.implicits._
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("text")
+      .option("path", "input")
+      .load()
+
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+    // Generate running word count
+    val wordCounts = words.groupBy().count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+}
